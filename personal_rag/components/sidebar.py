d2f77a6d854a47e66e5e863661b12f88d77574e1
Lena Yu <56573422+papafranco69@users.noreply.github.com>
2024-07-26 15:50:07 +0900

    Add files via upload

---
+++ personal_rag/components/sidebar.py
@@ -0,0 +1,28 @@
+import streamlit as st
+
+from components.faq import faq
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+
+def sidebar():
+    with st.sidebar:
+        st.markdown(
+            "## How to use\n"
+            "1. Upload a pdf, docx, or txt file📄\n"
+            "2. Ask a question about the document💬\n"
+        )
+        api_key_input = st.text_input(
+            "OpenAI API Key",
+            type="password",
+            placeholder="Paste your OpenAI API key here (sk-...)",
+            value=os.environ.get("OPENAI_API_KEY", None)
+            or st.session_state.get("OPENAI_API_KEY", ""),
+        )
+
+        st.session_state["OPENAI_API_KEY"] = api_key_input
+
+
+        faq()
---
+++ tests/unit_tests/fake_file.py
@@ -0,0 +1,10 @@
+from personal_rag.core.parsing import File
+from io import BytesIO
+
+
+class FakeFile(File):
+    """A fake file for testing purposes"""
+
+    @classmethod
+    def from_bytes(cls, file: BytesIO) -> "FakeFile":
+        return NotImplemented
